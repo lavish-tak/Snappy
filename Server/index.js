07a37ed60f825489e29f886901052d09b0d7d24f
lavish-tak <lavish1854@gmail.com>
2024-12-09 00:31:52 +0530

    register component made

---
+++ Server/index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+
+const app = express()
+require("dotenv").config()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+    console.log("DB connected successfully")
+})
+.catch((err)=>{
+    console.log(err)
+})
+
+const server = app.listen(process.env.PORT,()=>{
+    console.log(`Server started on port: ${process.env.PORT}`)
+})
---
+++ public/src/App.js
@@ -0,0 +1,19 @@
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+import './App.css';
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Chat from "./pages/Chat";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={<Chat/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
