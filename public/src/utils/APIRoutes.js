22a3d662e91ec5e6779c7cfc86d7043b57770fb3
lavish-tak <lavish1854@gmail.com>
2024-12-11 12:28:19 +0530

    Set Avatar working

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,3 +1,5 @@
 export const host = "http://localhost:3009"
 export const registerRoute = `${host}/api/auth/register`
 export const  loginRoute = `${host}/api/auth/login`
+export const  setAvatarRoute = `${host}/api/auth/setAvatar`
+export const allUsersRoute = `${host}/api/auth/allUsers`
